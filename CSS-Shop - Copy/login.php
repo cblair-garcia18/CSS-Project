6a0a9a43199fc68e88f1cdc7e564bc2080e4c428
unknown <emorrison19@LAB-SCI-304-08.whitworth.edu>
2016-04-27 21:22:35 -0700

    Copy with working Image scroll

--- CSS-Shop - Copy/login.php
+++ CSS-Shop - Copy/login.php
@@ -1,13 +1,14 @@
 <!DOCTYPE html>
 <html>
 
+
+
 <?php
 $cookie_name = "username";
 $cookie_value = "Erin Morrison";
 setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/");
 ?>
 <html>
-
 <body>
 
 <?php
@@ -20,22 +21,28 @@ if(!isset($_COOKIE[$cookie_name])) {
 
 </body>
 </html>
+<head id= "grad">
 
-<script type="text/javascript">
+<link rel="stylesheet" type="text/css" href="css/menus.css">
+<script type="text/javascript" src="javascript/login.js">-->
 
-var username, password;
+<!--<script type="text/javascript"> -->
+
+var username, password, testuser, testpass;
 
 function values()
 {
-	username= document.getElementById("username");
-	password= document.getElementById("password");
+	username = document.getElementById("username");
+	password = document.getElementById("password");
+	testuser = "emorrison19";
+	testpass = "Thisisatest6";
 }
 
 function login()
 {
-	if (username = "emorrison19")
+	if (username == testuser)
 	{
-		if (password = "Thisisatest6")
+		if (password == testpass)
 		{
 			alert("Welcome!");
 			window.location.href = 'file:///Z:/public_html/repos/CSS-Project/CSS-Shop/index.html';
@@ -55,18 +62,19 @@ function login()
 
 
 
-<head>
-
-<link rel="stylesheet" type="text/css" href="css/menus.css">
 
 
 
-<title>Custom Snow Shop - Shop for Snowboarding Gear That's Taylored To Your Style</title>
+
+
+
+<title>Custom Snow Shop - Shop for Snowboarding Gear That's Tailored To Your Style</title>
 </head>
 <h1>
-	<a href="index.html" class="logo"> 
+
+		<a href="index.html" class="logo"> 
 			<img src="images/logo.png" alt="Logo" style="width:170px;height:63px;border:0">
-	</a>
+		</a>
 
 		<div id="search">
 		<form id="searchbox" method="get">
@@ -76,18 +84,16 @@ function login()
 		</form>
 		</div>
 <div style="float: right">
-	
-	
-
 
 	<a href="register.html">Sign Up</a>
-	<a href="login.html">Log In</a>
+	<a href="login.php">Log In</a>
 	
 	<a href="cart.html" class="cart">
 		<img src="images/cart.png" alt="Your Cart" style="width:42px;height:42px;border:0">
 	</a>
 	
 </div>	
+
 <div style="clear: both"></div>
   
 </h1>
@@ -99,19 +105,18 @@ function login()
 
 <div class="ad">Ad Space</div>
 
-<form action="profile-setup.html">
-<div style="padding-bottom: 10px; padding-top: 5cm">
-<input type="email" id="email" placeholder="Enter your e-mail" required>
-<input type="email" id="confirmemail" placeholder="Confirm your e-mail" required>
-</div>
-<div style="padding-bottom: 50px">
-<input type="password" id="inputpassword" placeholder="Enter password" requried>
-<input type="password" id="confirmpassword" placeholder="Confirm password" required>
-</div>
-<div>
-<input type="submit">
-</div>
+
+<div style="padding-top: 5cm">
+
+
+<form action="logincheck.php">
+ <input type="text" id="username" placeholder="Username..." required>
+ <input type="password" id="password" placeholder="Password..." required>
+ <input type="button" onclick="login()" value="Log In">
 </form>
+</div>
+
+
 </body>
 
 </html>
---
+++ CSS-Shop - Copy/register.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html id="grad">
+
+<?php
+$cookie_name = "username";
+$cookie_value = "Erin Morrison";
+setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/");
+?>
+<html>
+
+<body>
+
+<?php
+if(!isset($_COOKIE[$cookie_name])) {
+    echo "Welcome back, " . $cookie_name . " " ;
+} else {
+    echo "Welcome to CSS, " . $_COOKIE[$cookie_name];
+}
+?>
+
+</body>
+</html>
+
+<script type="text/javascript">
+
+var username, password;
+
+function values()
+{
+	username= document.getElementById("username");
+	password= document.getElementById("password");
+}
+
+function login()
+{
+	if (username = "emorrison19")
+	{
+		if (password = "Thisisatest6")
+		{
+			alert("Welcome!");
+			window.location.href = 'file:///Z:/public_html/repos/CSS-Project/CSS-Shop/index.html';
+		}
+		else
+		{
+			alert("You have entered an incorrect username and/or password");
+		}
+	}
+	else
+		{
+			alert("You have entered an incorrect username and/or password");
+		}
+}
+
+</script>
+
+
+
+<head>
+
+<link rel="stylesheet" type="text/css" href="css/menus.css">
+
+
+
+<title>Custom Snow Shop - Shop for Snowboarding Gear That's Taylored To Your Style</title>
+</head>
+<h1>
+	<a href="index.html" class="logo"> 
+			<img src="images/logo.png" alt="Logo" style="width:170px;height:63px;border:0">
+	</a>
+
+		<div id="search">
+		<form id="searchbox" method="get">
+			<input type="search" id="txtSearch" name="search" placeholder="Search..."/>
+		
+			<input type="submit" value="Search"/>
+		</form>
+		</div>
+<div style="float: right">
+	
+	
+
+
+	<a href="register.html">Sign Up</a>
+	<a href="login.html">Log In</a>
+	
+	<a href="cart.html" class="cart">
+		<img src="images/cart.png" alt="Your Cart" style="width:42px;height:42px;border:0">
+	</a>
+	
+</div>	
+<div style="clear: both"></div>
+  
+</h1>
+
+
+
+<body>
+
+
+<div class="ad">Ad Space</div>
+
+<form action="profile-setup.html">
+<div style="padding-bottom: 10px; padding-top: 5cm">
+<input type="email" id="email" placeholder="Enter your e-mail" required>
+<input type="email" id="confirmemail" placeholder="Confirm your e-mail" required>
+</div>
+<div style="padding-bottom: 50px">
+<input type="password" id="inputpassword" placeholder="Enter password" requried>
+<input type="password" id="confirmpassword" placeholder="Confirm password" required>
+</div>
+<div>
+<input type="submit">
+</div>
+</form>
+</body>
+
+</html>
